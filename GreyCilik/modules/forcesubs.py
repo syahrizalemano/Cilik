a00536aee3d7baf79d942893f6f906b944da0462
Grey <95908917+grey423@users.noreply.github.com>
2022-04-13 07:25:21 -0700

    Update forcesubs.py

--- GreyCilik/modules/forcesubs.py
+++ GreyCilik/modules/forcesubs.py
@@ -179,20 +179,20 @@ def config(client, message):
 
 __help__ = """
 *Force Subscribe:*
-❂ Grey can mute members who are not subscribed your channel until they subscribe
-❂ When enabled I will mute unsubscribed members and show them a unmute button. When they pressed the button I will unmute them
-❂*Setup*
+✪ Grey can mute members who are not subscribed your channel until they subscribe
+✪ When enabled I will mute unsubscribed members and show them a unmute button. When they pressed the button I will unmute them
+✪*Setup*
 *Only creator*
-❂ Add me in your group as admin
-❂ Add me in your channel as admin 
+✪ Add me in your group as admin
+✪ Add me in your channel as admin 
  
 *Commmands*
-❂ /fsub {channel username} - To turn on and setup the channel.
+✪ /fsub {channel username} - To turn on and setup the channel.
   💡Do this first...
-❂ /fsub - To get the current settings.
-❂ /fsub disable - To turn of ForceSubscribe..
+✪ /fsub - To get the current settings.
+✪ /fsub disable - To turn of ForceSubscribe..
   💡If you disable fsub, you need to set again for working.. /fsub {channel username} 
-❂ /fsub clear - To unmute all members who muted by me.
+✪ /fsub clear - To unmute all members who muted by me.
 *Federation*
 Everything is fun, until a spammer starts entering your group, and you have to block it. Then you need to start banning more, and more, and it hurts.
 But then you have many groups, and you don't want this spammer to be in one of your groups - how can you deal? Do you have to manually block it, in all your groups?\n
