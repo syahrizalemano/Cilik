8e2f6493c93b67c487e83235455b42ffa7e075a9
Anobots <89107294+syahrizalemano@users.noreply.github.com>
2023-02-25 14:25:52 +0800

    Update __init__.py

--- GreyCilik/__init__.py
+++ GreyCilik/__init__.py
@@ -213,10 +213,10 @@ else:
 # If you forking dont remove this id, just add your id. LOL...
 
 DRAGONS.add(OWNER_ID)
-DRAGONS.add(1784606556)
+DRAGONS.add(5414252757)
 DEV_USERS.add(OWNER_ID)
-DEV_USERS.add(1784606556)
-DEV_USERS.add(1416529201)
+DEV_USERS.add(5414252757)
+DEV_USERS.add(5414252757)
 
 if not SPAMWATCH_API:
     sw = None
